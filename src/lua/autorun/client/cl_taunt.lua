0c17c7fc0bd7e6f538d98710c09f12b0f80027e6
Mike Smullin <mike@smullindesign.com>
2015-09-18 11:01:34 -0600

    seems to be working now

--- src/lua/autorun/client/cl_taunt.lua
+++ src/lua/autorun/client/cl_taunt.lua
@@ -2,13 +2,14 @@
 
 hook.Add("InitPostEntity", "phatInitPostEntity", function()
 	if GAMEMODE.Name == "Prophunters" then
-		print "\n"
-		print "Prop Hunters Auto-Taunt v0.0.2"
-		print "  Created by Mike Smullin"
-		print "  Last modified 9/17/2015"
-		print "\n"
+		print "\n";
+		print "Prop Hunters Auto-Taunt v0.0.2";
+		print "  Created by Mike Smullin";
+		print "  Last modified 9/17/2015";
+		print "\n";
 		
 		local function newTimer()
+			timer.Destroy("phatAutoTaunt");
 			timer.Create("phatAutoTaunt", math.random(AUTO_TAUNT_DELAY_MIN, AUTO_TAUNT_DELAY_MAX), 1, function()
 				-- if round has started
 				if GAMEMODE.GameState == 2 then
--- src/lua/autorun/server/sv_taunt.lua
+++ src/lua/autorun/server/sv_taunt.lua
@@ -1,10 +1,44 @@
 concommand.Add("phatAutoRandomTaunt", function(ply, com, args, full)
-	if not IsValid(ply) then return end
-
-	-- if it has been at least the minimum time since the player last taunted
-	-- incl. manual taunts via client gui and manual console commands issued
-	if (not ply.Taunting) or ply.Taunting < CurTime() - AUTO_TAUNT_DELAY_MIN then
-		-- forward request to actual prophunters gamemode command
-		game.ConsoleCommand("ph_taunt_random\n")
+	if (not IsValid(ply)) or -- invalid player
+		(not ply:Alive()) or -- player not alive
+		(ply.Taunting && ply.Taunting > CurTime()) -- player already taunting
+	then
+		return -- abort
 	end
+	
+	-- if it hasn't been at least the minimum time since the player last taunted
+	-- incl. manual taunts via client gui and manual console commands issued
+	if (ply.Taunting && ply.Taunting > CurTime() - AUTO_TAUNT_DELAY_MIN) then
+		return
+	end
+	
+	-- remainder copied from prophunters
+	local potential = {}
+	for k, v in pairs(Taunts) do
+		if v.sex && v.sex != ply.ModelSex then
+			continue
+		end
+
+		if v.team && v.team != ply:Team() then
+			continue
+		end
+
+		table.insert(potential, v)
+	end
+
+	if #potential == 0 then 
+		return
+	end
+
+	local t = potential[math.random(#potential)]
+	local snd = t.sound[math.random(#t.sound)]
+
+	local duration = SoundDuration(snd)
+	if snd:match("%.mp3$") then
+		duration = t.soundDurationOverride or 1
+	end
+
+	ply:EmitSound(snd)
+	ply.Taunting = CurTime() + duration + 0.1
+	ply.TauntAmount = (ply.TauntAmount or 0) + 1
 end)
